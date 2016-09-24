1e33dd0c2a3376587f1f601d905c186dbec80595
zhaokeli <735579768@qq.com>
2016-09-24 16:44:50 +0800

    修改缓存目录

--- index.php
+++ index.php
@@ -10,10 +10,14 @@
 // +----------------------------------------------------------------------
 
 // [ 应用入口文件 ]
+//站点入口文件根路径
+define('SITE_PATH', __DIR__);
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/apps/');
 // 绑定当前访问到index模块
 define('BIND_MODULE', 'index');
+//应用运行时目录
+define('RUNTIME_PATH', SITE_PATH . '/data/runtime/');
 // 加载框架引导文件
 require __DIR__ . '/tp5/start.php';
