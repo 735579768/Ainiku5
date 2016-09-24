d54013077675cc8923558b6e5eb4f1d8008448bb
zhaokeli <735579768@qq.com>
2016-09-24 17:38:50 +0800

    添加模板替换配置变量

--- apps/index/config.php
+++ apps/index/config.php
@@ -4,8 +4,8 @@ return [
 	// 视图输出字符串内容替换
 	'view_replace_str' => [
 		'__STATIC__' => '/public/static',
-		'__IMG__'    => '/public/admin/images',
-		'__CSS__'    => '/public/admin/css',
-		'__JS__'     => '/public/admin/js',
+		'__IMG__'    => '/public/index/images',
+		'__CSS__'    => '/public/index/css',
+		'__JS__'     => '/public/index/js',
 	],
 ];
