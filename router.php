8a1eb89d7596b6d22978ad38763cf1a9eac87d7a
zhaokeli <735579768@qq.com>
2016-09-24 17:09:20 +0800

    添加备注

--- router.php
+++ router.php
@@ -9,9 +9,8 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 // $Id$
-
 if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"])) {
-    return false;
+	return false;
 } else {
-    require __DIR__ . "/index.php";
+	require __DIR__ . "/index.php";
 }
