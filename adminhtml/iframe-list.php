1d4a034479ee95fefe3c6eb8ba2a52cc6f0be886
zhaokeli <735579768@qq.com>
2016-09-25 23:11:06 +0800

    添加主标题样式

---
+++ adminhtml/iframe-list.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<title>管理后台</title>
+<meta name="keywords" content="管理后台" />
+<meta name="description" content="管理后台" />
+<?php include "./include/head.php";?>
+</head>
+<body>
+<div class="iframe-con">
+	<div class="main-title">
+		<i class="fa fa-bars"></i>
+		<span class="">文章列表</span>
+		<b>></b>
+		<span onclick="window.history.go(-1);">返回</span>
+	</div>
+	<table width="100%">
+		<tr>
+			<th>Id</th>
+			<th>标题</th>
+			<th>时间</th>
+			<th>状态</th>
+			<th>操作</th>
+		</tr>
+		<tr>
+			<td>100</td>
+			<td class="td-left">最近的新闻动态</td>
+			<td>2016-09-26 12:01:54</td>
+			<td>正常</td>
+			<td class="td-btn"><a href="javascript:;" class="btn">编辑</a></td>
+		</tr>
+		<tr>
+			<td>100</td>
+			<td class="td-left">最近的新闻动态</td>
+			<td>2016-09-26 12:01:54</td>
+			<td>正常</td>
+			<td class="td-btn"><a href="javascript:;" class="btn">编辑</a></td>
+		</tr>
+		<tr>
+			<td>100</td>
+			<td class="td-left">最近的新闻动态</td>
+			<td>2016-09-26 12:01:54</td>
+			<td>正常</td>
+			<td class="td-btn"><a href="javascript:;" class="btn">编辑</a></td>
+		</tr>
+		<tr>
+			<td>100</td>
+			<td class="td-left">最近的新闻动态</td>
+			<td>2016-09-26 12:01:54</td>
+			<td>正常</td>
+			<td class="td-btn"><a href="javascript:;" class="btn">编辑</a></td>
+		</tr>
+		<tr>
+			<td>100</td>
+			<td class="td-left">最近的新闻动态</td>
+			<td>2016-09-26 12:01:54</td>
+			<td>正常</td>
+			<td class="td-btn"><a href="javascript:;" class="btn">编辑</a></td>
+		</tr>
+	</table>
+</div>
+</body>
+</html>
--- adminhtml/include/left.php
+++ adminhtml/include/left.php
@@ -3,10 +3,10 @@
 <div id="left-menu">
 	<dl class="menu">
 		<dt class="menu-title">
-			<a href="javascript:;" onclick="am.menuTitleClick(this);" ><i class="fa menu-close"></i>我的博客</a>
+			<a href="javascript:;" onclick="am.menuTitleClick(this);" ><i class="fa menu-close"></i>默认</a>
 		</dt>
 		<dd class="menu-item">
-			<a href="javascript:;" data-url="https://www.zhaokeli.com/" onclick="am.menuClick(this);">我的博客</a>
+			<a href="javascript:;" data-url="./iframe-list.php" onclick="am.menuClick(this);">文章列表</a>
 		</dd>
 		<dd class="menu-item">
 			<a href="javascript:;" data-url="https://www.zhaokeli.com/" onclick="am.menuClick(this);">我的博客</a>
