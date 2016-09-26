b2647c2e5e4dac73d5d901af7cec5848302b06e1
735579768@qq.com <735579768@qq.com>
2016-09-26 15:03:59 +0800

    完善头部样式

--- adminhtml/include/header.php
+++ adminhtml/include/header.php
@@ -1,5 +1,25 @@
 <div id="admin-header" class="admin-header">
- <div class="touxiang">
- 	<img src="../images/touxiang.jpg">
- </div>
+    <div class="touxiang">
+        <img src="../images/touxiang.jpg">
+    </div>
+    <ul id="mainnav" class="mainnav cl">
+        <li><a href="javascript:;">首页</a></li>
+        <li><a href="javascript:;">文档</a></li>
+        <li><a href="javascript:;">产品</a></li>
+        <li><a href="javascript:;">文章</a></li>
+        <li><a href="javascript:;">用户</a></li>
+        <li><a href="javascript:;">系统</a></li>
+        <li><a href="javascript:;">扩展</a></li>
+    </ul>
+    <ul class="tool cl">
+        <li><a href="javascript:;" title="(超级管理员),登陆地址:(127.0.0.1)本机地址">您好:爱你酷</a></li>
+        <li><a href="javascript:;" target="_blank"><i class="fa fa-home"></i>查看网站</a></li>
+        <li class="downmenu"><a href="javascript:;"><i class="fa fa-user"></i>个人信息</a>
+            <ul class="pwdinfo">
+                <li><a href="javascript:;">清空缓存</a></li>
+                <li><a href="javascript:;">修改密码</a></li>
+                <li><a href="javascript:;">退出系统</a></li>
+            </ul>
+        </li>
+    </ul>
 </div>
