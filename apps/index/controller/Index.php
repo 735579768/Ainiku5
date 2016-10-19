3a08ba14653e4affcf82da16a59f444ece250182
zhaokeli <735579768@qq.com>
2016-10-19 22:59:28 +0800

    添加路由功能

--- apps/index/controller/Index.php
+++ apps/index/controller/Index.php
@@ -5,7 +5,7 @@ class Index extends Base {
 	public function index() {
 		// config('template.view_path', 'default');
 		// $this->view->config('view_path', 'default');
-		// dump($this->view);
+		// dump(config(''));
 		return $this->fetch();
 	}
 }
--- apps/route.php
+++ apps/route.php
@@ -12,7 +12,7 @@
 // \think\Route::domain('ainiku5.loc', 'admin');
 return [
 	//域名绑定
-	'__domain__'  => [
+	'__domain__'            => [
 		''     => 'admin',
 		//后台
 		'user' => 'admin',
@@ -22,12 +22,18 @@ return [
 		// '*.user' => 'user',
 		// '*'      => 'book',
 	],
-	'__pattern__' => [
+	'__pattern__'           => [
 		'name' => '\w+',
 	],
-	'[hello]'     => [
+	'[hello]'               => [
 		':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
 		':name' => ['index/hello', ['method' => 'post']],
 	],
+
+	'/'                     => 'index', // 首页访问路由
+	'my'                    => 'Article/detail', // 静态地址路由
+	'blog/:id'              => 'Blog/read', // 静态地址和动态地址结合
+	'new/:year/:month/:day' => 'News/read', // 静态地址和动态地址结合
+	':user/:blog_id'        => 'Blog/read', // 全动态地址
 
 ];
