f341d0994eb0ed6b5de51f39998e299a668e69d5
735579768@qq.com <735579768@qq.com>
2016-10-25 15:38:42 +0800

    添加调试环境配置

--- apps/extra/database.php
+++ apps/extra/database.php
@@ -31,7 +31,7 @@ return [
 	// 数据库表前缀
 	'prefix'         => 'kl_',
 	// 数据库调试模式
-	'debug'          => true,
+	'debug'          => false,
 	// 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
 	'deploy'         => 0,
 	// 数据库读写是否分离 主从式有效
@@ -47,5 +47,5 @@ return [
 	// 是否自动写入时间戳字段
 	'auto_timestamp' => true,
 	// 是否需要进行SQL性能分析
-	'sql_explain'    => true,
+	'sql_explain'    => false,
 ];
