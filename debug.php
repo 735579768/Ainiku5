e2b9fe0916042c64b8ce8b38b142a68bce01a102
zhaokeli <735579768@qq.com>
2016-11-27 22:29:01 +0800

    fex

--- debug.php
+++ debug.php
@@ -22,21 +22,24 @@ return [
 		'__pattern__'         => [
 			'name' => '\w+',
 		],
+		//路由分组
 		// '[hello]'               => [
 		// 	':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
 		// 	':name' => ['index/hello', ['method' => 'post']],
 		// ],
+		//原路由
+		// 'blog/:id'   => ['Blog/read', ['method' => 'get'], ['id' => '\d+']],
+		// 'blog/:name' => ['Blog/read', ['method' => 'post']],
 
-		'/'                   => 'index', // 首页访问路由
-		'cat/:fenlei'         => 'article/index',
-		'fenlei/:fenlei/p/:p' => 'article/index',
-		'tag/:tagid'          => 'article/taglist',
-		'p/:p'                => 'index/index',
-		'article/:article_id' => 'article/detail', // 静态地址路由
-		'alist/:category_id'  => 'article/index',
-		'glist/:category_id'  => 'goods/index',
-		'adetail/:article_id' => 'article/detail', // 静态地址路由
-		'gdetail/:goods_id'   => 'goods/detail', // 静态地址路由
+		'cat/:fenlei'         => 'home/article/index',
+		'fenlei/:fenlei/p/:p' => 'home/article/index',
+		'tag/:tagid'          => 'home/article/taglist',
+		'p/:p'                => 'home/index/index',
+		'article/:article_id' => 'home/article/detail', // 静态地址路由
+		'alist/:category_id'  => 'home/article/index',
+		'glist/:category_id'  => 'home/goods/index',
+		'adetail/:article_id' => 'home/article/detail', // 静态地址路由
+		'gdetail/:goods_id'   => 'home/goods/detail', // 静态地址路由
 		// 'blog/:id'              => 'blog/read', // 静态地址和动态地址结合
 		// 'new/:year/:month/:day' => 'news/read', // 静态地址和动态地址结合
 		// ':user/:blog_id'        => 'blog/read', // 全动态地址
