5ddfdf30a7868a8d31482f4e4c80148ceffa7a2d
zhaokeli <735579768@qq.com>
2016-11-29 20:27:34 +0800

    add

--- apps/home/controller/Tool.php
+++ apps/home/controller/Tool.php
@@ -2,12 +2,10 @@
 namespace app\home\controller;
 
 class Tool extends Base {
-public function tiaose()
-{
-return $this->fetch();
-}
-public function css()
-{
-return $this->fetch();
-}
+	public function tiaose() {
+		return $this->fetch();
+	}
+	public function css() {
+		return $this->fetch();
+	}
 }
