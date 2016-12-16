062153fb1b9ed6083e7944ec6ce1f8683d745a71
735579768@qq.com <735579768@qq.com>
2016-12-16 16:02:46 +0800

    fix

--- apps/index/controller/Pub.php
+++ apps/index/controller/Pub.php
@@ -101,6 +101,6 @@ class Pub extends \app\common\Controller\Base {
 	public function loginOut() {
 		session(null);
 		cookie(null);
-		$this->success('退出成功');
+		$this->success('退出成功', url('/'));
 	}
 }
