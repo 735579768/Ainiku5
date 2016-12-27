d970c33859e38a9ae13ad9fc9c6df2ce2cb6196a
zhaokeli <735579768@qq.com>
2016-12-27 19:06:31 +0800

    添加index.php重定向

--- vendor/extend/ank/Page.php
+++ vendor/extend/ank/Page.php
@@ -157,10 +157,10 @@ class Page {
 		}
 
 		//替换分页内容
-		$page_str = str_replace(
+		$page_str = trim(str_replace(
 			array('%HEADER%', '%NOW_PAGE%', '%UP_PAGE%', '%DOWN_PAGE%', '%FIRST%', '%LINK_PAGE%', '%END%', '%TOTAL_ROW%', '%TOTAL_PAGE%'),
 			array($this->config['header'], $this->nowPage, $up_page, $down_page, $the_first, $link_page, $the_end, $this->totalRows, $this->totalPages),
-			$this->config['theme']);
-		return "<div class='page-inner'>{$page_str}</div>";
+			$this->config['theme']));
+		return "{$page_str}";
 	}
 }
